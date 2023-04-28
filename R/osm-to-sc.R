5cf04e8ae5ca9197462c80e6cc884af302c31869
mpadge <mark.padgham@email.com>
2023-04-28 10:57:34 +0200

    fix osm_to_sc with single network file

--- R/osm-to-sc.R
+++ R/osm-to-sc.R
@@ -6,17 +6,14 @@
 #' @param path Path to the directory containing various '.osm' files produced by
 #' \link{uta_extract_osm}.
 #' @param city Name of city (used to name resultant files).
-#' @param remove_osm_files If `TRUE` (default), remove the individual '.osm'
-#' files for each key after conversion to \pkg{silicate} format.
 #' @return A single \pkg{silicate} 'SC' object containing combined data from all
 #' individual '.osm' files. This file is also saved as a '.Rds' object in the
 #' 'path' directory.
 #'
 #' @export
 
-uta_osm_to_sc <- function (path, city, remove_osm_files = TRUE) {
+uta_osm_to_sc <- function (path, city) {
 
-    requireNamespace ("fs")
     requireNamespace ("fs")
 
     checkmate::assert_character (city, min.len = 1L, max.len = 1L)
@@ -25,66 +22,31 @@ uta_osm_to_sc <- function (path, city, remove_osm_files = TRUE) {
     f_sc <- fs::path (path, paste0 (city, "-sc.Rds"))
     if (fs::file_exists (f_sc)) {
         cli::cli_alert_info (cli::col_blue (
-            "File '",
-            f_sc,
-            "' already exists and will not be overwritten."
+            "File '{f_sc}' already exists and will not be overwritten."
         ))
         return ()
     }
 
     q <- osmdata::opq (get_pbf_bbox (path, city))
 
-    flist <- fs::dir_ls (path, regexp = paste0 (city, ".*\\.osm$"))
-    flist <- flist [which (!grepl ("\\-parking\\.", flist))]
-    if (length (flist) == 0L) {
+    f <- fs::dir_ls (path, regexp = paste0 (city, ".*\\.osm$"))
+    f <- f [which (!grepl ("\\-(parking|schools)\\.", f))]
+    if (length (f) == 0L) {
         cli::cli_alert_warning (cli::col_red (
             "No '.osm' files found; did you run 'uta_extract_osm'?"
         ))
         return ()
     }
-
-    dat <- osmdata::osmdata_sc (q, doc = flist [1])
-    cli::cli_text (
-        "[1 / ", length (flist), "]: ",
-        gsub (paste0 ("^", city, "\\-"), "", flist [1])
-    )
-
-    count <- 2L
-    for (f in flist [-1]) {
-        cli::cli_text (
-            "[", count, " / ", length (flist), "]: ",
-            gsub (paste0 ("^", city, "\\-"), "", f)
-        )
-        count <- count + 1L
-        i <- osmdata::osmdata_sc (q, doc = f)
-
-        dat$nodes <- rbind (dat$nodes, i$nodes)
-        dat$relation_members <- rbind (dat$relation_members, i$relation_members)
-        dat$relation_properties <-
-            rbind (dat$relation_properties, i$relation_properties)
-        dat$object <- rbind (dat$object, i$object)
-        dat$object_link_edge <- rbind (dat$object_link_edge, i$object_link_edge)
-        dat$edge <- rbind (dat$edge, i$edge)
-        dat$vertex <- rbind (dat$vertex, i$vertex)
+    if (length (f) > 1L) {
+        cli::cli_alert_warning (cli::col_red (
+            "Multiple network files found: {f}"
+        ))
     }
 
-    dat$nodes <- unique (dat$nodes)
-    dat$relation_members <- unique (dat$relation_members)
-    dat$relation_properties <- unique (dat$relation_properties)
-    dat$object <- unique (dat$object)
-    dat$object_link_edge <- unique (dat$object_link_edge)
-    dat$edge <- unique (dat$edge)
-    dat$vertex <- unique (dat$vertex)
-
-    if (remove_osm_files) {
-        fs::file_delete (flist)
-    }
-
+    dat <- osmdata::osmdata_sc (q, doc = f [1])
     saveRDS (dat, f_sc)
     cli::cli_alert_success (cli::col_green (
-        "All OSM data collated and written to '",
-        f_sc,
-        "'"
+        "All OSM data collated and written to '{f_sc}'"
     ))
 
     return (dat)
