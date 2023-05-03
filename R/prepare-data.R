95ded02d7d34e0f26aef18b167591451fa32986a
mpadge <mark.padgham@email.com>
2023-05-03 11:25:26 +0200

    add 'reproj_equal_area' fn for #12

--- R/prepare-data.R
+++ R/prepare-data.R
@@ -43,8 +43,15 @@ prepare_natural <- function (f, city, water_dist = 20) {
     }
 
     natural <- sf::st_read (f, layer = "multipolygons")
+    natural <- reproj_equal_area (natural)
     polys <- lapply (natural$geometry, function (i) i [[1]] [[1]])
-    v <- m4ra::m4ra_vertices (net, city)
+
+    v <- m4ra::m4ra_vertices (net, city) [, c ("x", "y")]
+    v <- sfheaders::sf_point (v)
+    sf::st_crs (v) <- 4326
+    v <- reproj_equal_area (v)
+    v <- data.frame (sf::st_coordinates (v))
+    names (v) <- c ("x", "y")
 
     index <- cpp_pip (polys, v) + 1L
 
@@ -60,6 +67,26 @@ prepare_natural <- function (f, city, water_dist = 20) {
     saveRDS (index, f_natural)
 
     return (f_natural)
+}
+
+#' Re-project on to Lambert Azimuthal equal-area projection, centred at center
+#' of actual objects.
+#' @noRd
+reproj_equal_area <- function (x) {
+
+    xy <- sf::st_coordinates (x)
+    xy <- round (apply (xy, 2, mean))
+
+    crs <- sf::st_crs (paste0 (
+        "+proj=laea +lat_0=",
+        xy [1],
+        " +lon_0=",
+        xy [2],
+        " +x_0=4321000 +y_0=3210000 +ellps=GRS80 ",
+        "+towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+    ))
+
+    sf::st_transform (x, crs = crs)
 }
 
 prepare_schools <- function (f, city) {
