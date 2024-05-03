af9447a8e920ac0e443e5157470fd9cbdd5e816b
mpadge <mark.padgham@email.com>
2024-05-03 14:48:47 +0200

    mv 'reproj_equal_area' -> utils.R

--- R/prepare-data.R
+++ R/prepare-data.R
@@ -91,26 +91,6 @@ prepare_natural <- function (f, city, water_dist = 20, reproj = FALSE) {
     return (f_natural)
 }
 
-#' Re-project on to Lambert Azimuthal equal-area projection, centred at center
-#' of actual objects.
-#' @noRd
-reproj_equal_area <- function (x) {
-
-    xy <- sf::st_coordinates (x)
-    xy <- round (apply (xy, 2, mean))
-
-    crs <- sf::st_crs (paste0 (
-        "+proj=laea +lat_0=",
-        xy [1],
-        " +lon_0=",
-        xy [2],
-        " +x_0=4321000 +y_0=3210000 +ellps=GRS80 ",
-        "+towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
-    ))
-
-    sf::st_transform (x, crs = crs)
-}
-
 prepare_schools <- function (f, city) {
 
     cache_dir <- fs::path (m4ra_cache_dir (), city)
